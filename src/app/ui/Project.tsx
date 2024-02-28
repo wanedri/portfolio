ab6ee21a93fbe7484fa364bff8b11b99a94116be
wanedri <50620343+wanedri@users.noreply.github.com>
2024-02-29 01:56:25 +0800

    fix call lib tilt

--- src/app/ui/Project.tsx
+++ src/app/ui/Project.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React from "react";
-import {Tilt}  from "react-tilt";
+import Tilt  from "react-tilt";
 import { motion } from "framer-motion";
 import { styles } from "../styles";
 // import { github } from "../assets";
